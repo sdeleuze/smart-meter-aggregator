45f2bc22db0908954f252822c2d0dda5c5c8d5cb
Stephane Nicoll <snicoll@pivotal.io>
2017-10-25 10:27:14 +0200

    Improve randomness of sensor data

--- src/main/java/com/example/meter/aggregator/generator/ElectricityMeasureFluxGenerator.java
+++ src/main/java/com/example/meter/aggregator/generator/ElectricityMeasureFluxGenerator.java
@@ -3,6 +3,7 @@ package com.example.meter.aggregator.generator;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.Random;
 import java.util.UUID;
 
 import com.example.meter.aggregator.domain.ElectricityMeasure;
@@ -10,6 +11,8 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 class ElectricityMeasureFluxGenerator {
+
+	private static final Random random = new Random();
 
 	private final ZoneInfo zoneInfo;
 
@@ -27,13 +30,18 @@ class ElectricityMeasureFluxGenerator {
 						Flux.range(1, this.zoneInfo.getDevicesCount())
 								.map(sensorIndex
 										-> generateMeasure(sensorIndex, reportTimestamp))
-								.delayUntil(i -> Mono.delay(Duration.ofMillis(50))))
+								.delayUntil(i -> randomDelay()))
 				.share();
 	}
 
 	private ElectricityMeasure generateMeasure(int sensorIndex, long reportTimestamp) {
 		return new ElectricityMeasure(generateSensorId(sensorIndex), this.zoneInfo.getZoneId(),
 				reportTimestamp, this.zoneInfo.randomPower());
+	}
+
+	private Mono<Long> randomDelay() {
+		int max = 10000 / this.zoneInfo.getDevicesCount();
+		return Mono.delay(Duration.ofMillis(10 + random.nextInt(max)));
 	}
 
 	private String generateSensorId(long sensorIndex) {
