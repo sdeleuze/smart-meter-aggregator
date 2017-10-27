fc5067357e61403812cb7241ffea4e8781d16237
Sebastien Deleuze <sdeleuze@pivotal.io>
2017-10-27 09:46:45 +0200

    Add @NonNull annotation on API (null-safety warnings remain)

--- src/main/java/com/example/meter/aggregator/web/ElectricityMeasureHandler.java
+++ src/main/java/com/example/meter/aggregator/web/ElectricityMeasureHandler.java
@@ -8,6 +8,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import org.springframework.http.MediaType;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -19,17 +20,19 @@ class ElectricityMeasureHandler {
 
 	private final Flux<ElectricityMeasure> firehose;
 
-	ElectricityMeasureHandler(ElectricityMeasureGenerator generator) {
+	ElectricityMeasureHandler(@NonNull ElectricityMeasureGenerator generator) {
 		this.content = generator.generateSensorData();
 		this.firehose = Flux.merge(content.values()).share();
 	}
 
-	public Mono<ServerResponse> firehose(ServerRequest request) {
+	@NonNull
+	public Mono<ServerResponse> firehose(@NonNull ServerRequest request) {
 		return ServerResponse.ok().contentType(MediaType.APPLICATION_STREAM_JSON)
 				.body(this.firehose, ElectricityMeasure.class);
 	}
 
-	public Mono<ServerResponse> forZone(ServerRequest request) {
+    @NonNull
+	public Mono<ServerResponse> forZone(@NonNull ServerRequest request) {
 		String id = request.pathVariable("id");
 		return ServerResponse.ok().contentType(MediaType.APPLICATION_STREAM_JSON)
 				.body(this.content.get(id), ElectricityMeasure.class);
